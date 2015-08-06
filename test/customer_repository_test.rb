792775222b83aa37bcd676d6e3195802f8697080
Bret Doucette <doucette.bret@gmail.com>
2015-08-05 20:20:09 -0600

    Fixed Repo Tests

--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -8,7 +8,7 @@ class CustomerRepositoryTest < Minitest::Test
 
   def setup
     customer1 = Customer.new({id: '1', first_name: 'Schroeder', last_name: 'Jerde', created_at: '2012-03-27 14:53:59 UTC', updated_at: '2012-03-27 14:53:59 UTC'}, self)
-    customer2 = Customer.new({id: '2', fist_name: 'Timothy', last_name: 'Bigsby', created_at: '2012-02-11 13:43:29 UTC', updated_at: '2011-06-27 14:53:59 UTC'}, self)
+    customer2 = Customer.new({id: '2', first_name: 'Timothy', last_name: 'Bigsby', created_at: '2012-02-11 13:43:29 UTC', updated_at: '2011-06-27 14:53:59 UTC'}, self)
     customer3 = Customer.new({id: '', first_name: '', last_name: '', created_at: '', updated_at: ''}, self)
     @cr = CustomerRepository.new([customer1, customer2, customer3])
   end
@@ -43,66 +43,67 @@ class CustomerRepositoryTest < Minitest::Test
     assert_equal Customer, @cr.find_by_updated_at('2011-06-27 14:53:59 UTC').class
   end
 
-  # def test_it_can_find_a_customer_by_first_name
-  #   assert_equal "Timothy", @cr.find_by_first_name("Timothy").first_name
-  # end
+  def test_it_can_find_a_customer_by_first_name
+    assert_equal "Timothy", @cr.find_by_first_name("Timothy").first_name
+  end
 
-  # def test_it_can_find_a_customer_by_last_name
-  #   assert_equal "Bigsby", @cr.find_by_first_name("Bigsby").last_name
-  # end
+  def test_it_can_find_a_customer_by_last_name
+    assert_equal "Bigsby", @cr.find_by_last_name("Bigsby").last_name
+  end
 
-  # def test_it_can_find_a_customer_case_insensitive_first_name
-  #   assert_equal "Timothy", @cr.find_by_last_name('timothy').first_name
-  # end
+  def test_it_can_find_a_customer_case_insensitive_first_name
+    assert_equal "Timothy", @cr.find_by_first_name('timothy').first_name
+  end
 
   # def test_it_can_find_a_customer_case_insensitive_last_name
   #   assert_equal "Bigsby", @cr.find_by_last_name('bigsby').last_name
   # end
 
-  # def test_it_does_not_know_first_nicknames
-  #   refute @cr.find_by_first_name('Tim')
-  # end
+  def test_it_does_not_know_first_nicknames
+    refute @cr.find_by_first_name('Tim')
+  end
 
-  # def test_it_does_not_know_last_nicknames
-  #   refute @cr.find_by_last_name('Bigs')
-  # end
+  def test_it_does_not_know_last_nicknames
+    refute @cr.find_by_last_name('Bigs')
+  end
 
-  # def test_it_can_find_a_customer_by_id
-  #   assert_equal '2', @cr.find_by_id("2").id
-  # end
+  def test_it_can_find_a_customer_by_id
+    assert_equal '2', @cr.find_by_id("2").id
+  end
 
-  # def test_it_can_find_a_customer_by_created_at
-  #   assert_equal '2012-02-11 13:43:29 UTC', @cr.find_by_created_at('2012-02-11 13:43:29 UTC').created_at
-  # end
+  def test_it_can_find_a_customer_by_created_at
+    assert_equal '2012-02-11 13:43:29 UTC', @cr.find_by_created_at('2012-02-11 13:43:29 UTC').created_at
+  end
 
-  # def test_it_can_find_a_customer_by_updated_at
-  #   assert_equal '2011-06-27 14:53:59 UTC', @cr.find_by_updated_at('2011-06-27 14:53:59 UTC').updated_at
-  # end
+  def test_it_can_find_a_customer_by_updated_at
+    assert_equal '2011-06-27 14:53:59 UTC', @cr.find_by_updated_at('2011-06-27 14:53:59 UTC').updated_at
+  end
 
-  # def test_it_can_find_all_customers_by_first_name
-  #   assert_equal 1, @cr.find_all_by_first_name("Timothy").count
-  # end
+  def test_it_can_find_all_customers_by_first_name
+    assert_equal 1, @cr.find_all_by_first_name("Timothy").count
+  end
 
-  # def test_it_can_find_all_customers_by_last_name
-  #   assert_equal 1, @cr.find_all_by_last_name("Bigsby").count
-  # end
+  def test_it_can_find_all_customers_by_last_name
+    assert_equal 1, @cr.find_all_by_last_name("Bigsby").count
+  end
 
-  # def test_it_can_find_all_customers_by_id
-  #   assert_equal 1, @cr.find_all_by_id("2").count
-  # end
+  def test_it_can_find_all_customers_by_id
+    assert_equal 1, @cr.find_all_by_id("2").count
+  end
 
-  # def test_it_can_find_all_customers_by_created_at
-  #   assert_equal 1, @cr.find_all_by_created_at('2012-02-11 13:43:29 UTC').count
-  # end
+  def test_it_can_find_all_customers_by_created_at
+    assert_equal 1, @cr.find_all_by_created_at('2012-02-11 13:43:29 UTC').count
+  end
 
-  # def test_it_can_find_all_customers_by_updated_at
-  #   assert_equal 1, @cr.find_all_by_updated_at('2011-06-27 14:53:59 UTC').count
-  # end
+  def test_it_can_find_all_customers_by_updated_at
+    assert_equal 1, @cr.find_all_by_updated_at('2011-06-27 14:53:59 UTC').count
+  end
 
-  # def test_it_can_return_an_empty_array_when_there_is_no_value
-  #   assert_equal [], @cr.find_all_by_name('')
-  #   assert_equal [], @cr.find_all_by_id('')
-  #   assert_equal [], @cr.find_all_by_created_at('')
-  #   assert_equal [], @cr.find_all_by_updated_at('')
-  # end
+  def test_it_can_return_an_empty_array_when_there_is_no_value
+    assert_equal [], @cr.find_all_by_first_name('')
+    assert_equal [], @cr.find_all_by_last_name('')
+    assert_equal [], @cr.find_all_by_id('')
+    assert_equal [], @cr.find_all_by_created_at('')
+    assert_equal [], @cr.find_all_by_updated_at('')
+  end
 end
